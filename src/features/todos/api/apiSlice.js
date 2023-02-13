8c06e5fa9686037f4397d053227ed1f94be09618
ol3gatron <olegatron@gmail.com>
2023-02-13 13:12:50 +0300

    created apiSlice and using it via provider

---
+++ src/features/todos/api/apiSlice.js
@@ -0,0 +1,15 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+  endpoints: (builer) => ({
+    getTodos: builer.query({
+      query: () => "/todos",
+    })
+  })
+})
+
+export const {
+  useGetTodosQuery
+} = apiSlice
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ApiProvider } from '@reduxjs/toolkit/dist/query/react';
+import { apiSlice } from './features/todos/api/apiSlice';
 
 ReactDOM.createRoot(document.getElementById('root'))
   .render(
     <React.StrictMode>
-      <App />
+      <ApiProvider api={apiSlice}>
+        <App />
+      </ApiProvider>
     </React.StrictMode>
   );
