594baf768fe8e0580c043ce1ee7a4b53156a6159
ol3gatron <olegatron@gmail.com>
2023-02-13 13:47:16 +0300

    added new methods to apiSlice

--- src/features/todos/api/apiSlice.js
+++ src/features/todos/api/apiSlice.js
@@ -6,6 +6,27 @@ export const apiSlice = createApi({
   endpoints: (builder) => ({
     getTodos: builder.query({
       query: () => "/todos",
+    }),
+    addTodo: builder.mutation({
+      query: (todo) => ({
+        url: "/todos",
+        method: "POST",
+        body: todo
+      })
+    }),
+    updateTodo: builder.mutation({
+      query: (todo) => ({
+        url: `/todos/${todo.id}`,
+        method: "PATCH",
+        body: todo
+      })
+    }),
+    deleteTodo: builder.mutation({
+      query: ({ id }) = ({
+        url: `/todos/${id}`,
+        method: "DELETE",
+        body: id
+      })
     })
   })
 })
